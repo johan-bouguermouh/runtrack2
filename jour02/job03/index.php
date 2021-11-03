67058fd948a74a02937c01986a36785c1eff9d9c
Johan <johan.bouguermouh@laplateforme.io>
2021-11-03 15:42:25 +0100

    Add Job03

---
+++ jour02/job03/index.php
@@ -0,0 +1,17 @@
+<?php
+$x = 1;
+$y = "Laplateforme_";
+while($x<=100){
+    if(($x>=25)&&($x<=50)&&($x != 42)){
+        echo "<i>$x</i>".'<br>';
+    }
+    elseif($x == 42){
+        echo $y.'<br>';
+    }
+    else{
+        echo $x . '<br>';
+    }
+    $x = $x + 1;
+}
+
+
