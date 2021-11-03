3783808453024ec5cd6f4afdf2cb885f6df67fa0
Johan <johan.bouguermouh@laplateforme.io>
2021-11-03 14:22:50 +0100

    add situation

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -1,6 +1,24 @@
 <?php
 $x = 0;
-while($x <= 1337){
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <p>Afficher tous les nombres compris entre 0 et 1337 en mettant un
+retour à la ligne entre chaque nombre. Le nombre 42 doit
+être en gras et souligné</p>
+    </header>
+    <main>
+  <?php
+        while($x <= 1337){
     if($x == 42){
         echo "<b><u>$x</u></b>".'<br>';
     }
@@ -8,4 +26,11 @@ while($x <= 1337){
     echo $x . '<br>';
     }
     $x = $x + 1;
-}
+    }
+ ?>
+    </main>
+    <footer>
+        <p>Johan Bouguermouh - Runtrack2<p> 
+    </footer>
+</body>
+</html>
