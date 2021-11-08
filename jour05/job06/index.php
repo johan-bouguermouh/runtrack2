f61abac05e54cc2092d705ad9f02f930f34b57e8
Johan <johan.bouguermouh@laplateforme.io>
2021-11-08 16:54:34 +0100

    add Day05 > Job06

---
+++ jour05/job06/index.php
@@ -0,0 +1,32 @@
+<?php
+$str="C'est partie pour un nouvel exercice !";
+
+function leetspeak($str){
+    $i = 0;
+    while(isset($str[$i])){
+        if($str[$i] == 'a'||$str[$i] == 'A'){
+            $str[$i] = 4;
+        }
+        elseif($str[$i] == 'e'||$str[$i] == 'E'){
+            $str[$i] = 3;
+        }
+        elseif($str[$i] == 's'||$str[$i] == 'S'){
+            $str[$i] = 5;
+        }
+        elseif($str[$i] == 't'||$str[$i] == 'T'){
+            $str[$i] = 7;
+        }
+        elseif($str[$i] == 'l'||$str[$i] == 'L'){
+            $str[$i] = 1;
+        }
+        elseif($str[$i] == 'b'||$str[$i] == 'B'){
+            $str[$i] = 8;
+        }
+        elseif($str[$i] == 'g'||$str[$i] == 'G'){
+            $str[$i] = 6;
+        }
+        $i++;
+    }
+    return $str;
+}
+echo leetspeak($str);
