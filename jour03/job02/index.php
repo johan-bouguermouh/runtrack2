65419e2c0e7a80d4fd1f512a5329167beeb68cc7
Johan <johan.bouguermouh@laplateforme.io>
2021-11-08 10:17:47 +0100

    Add Jour03 > Job02

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -4,13 +4,14 @@ $str="Tous ces instants
 seront perdus dans le temps comme les larmes sous la pluie";
 //  echo strlen($str) . '<br>';//77 caractère
 $caractère=1;
+$bool= false;
 
  while($caractère<=77){
-     if($caractère%2)
+     if($caractère%2==0)
      {
-         $str[$caractère] = ' ';
+        echo $str[$caractère];
      }
+
      $caractère = $caractère + 1;
  }
-  echo $str;
 ?>
