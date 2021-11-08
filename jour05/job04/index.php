b4991eba5eb80c2eb53cbed97c4a8bdbadd40a95
Johan <johan.bouguermouh@laplateforme.io>
2021-11-08 15:05:33 +0100

    Add Day05 > Job04

---
+++ jour05/job04/index.php
@@ -0,0 +1,28 @@
+
+<?php
+$a= 2;
+$b= 3;
+$operation="*";
+
+function calcule($a, $operation,$b)
+{
+    if($operation == "+"){
+        return($a * $b);
+    }
+    elseif($operation == "-"){
+        return($a - $b);
+    }
+    elseif($operation == "*"){
+        return($a * $b);
+    }
+    elseif($operation == "/"){
+        return($a / $b);
+    }
+    elseif($operation == "%"){
+        return($a % $b);
+    }
+
+
+}
+echo calcule($a,$operation,$b)
+?>
